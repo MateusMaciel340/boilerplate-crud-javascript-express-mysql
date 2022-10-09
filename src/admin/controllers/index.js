9ccb7d9ee90dab76a8544c925c16f62e977296d6
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-08 23:49:22 -0300

    feat: add capturing controller auth

--- src/admin/controllers/index.js
+++ src/admin/controllers/index.js
@@ -1,5 +1,7 @@
 const controllerUser = require("./user/controllerUser");
+const controllerAuth = require("./auth/controllerAuth");
 
 module.exports = {
-    controllerUser
+    controllerUser,
+    controllerAuth
 };
