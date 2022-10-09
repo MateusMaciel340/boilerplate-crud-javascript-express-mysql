02d80537fcc40dced2da0cbe53cda2cd4f10e04e
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-08 23:16:16 -0300

    feat: add searching user in controller user

--- src/admin/controllers/user/controllerUser.js
+++ src/admin/controllers/user/controllerUser.js
@@ -2,11 +2,26 @@ const models = require("../../../sequelize/models");
 
 module.exports = {
     listingUser: async (req, res) => {
-        try{
+        try {
             const listingUser = await models.User.findAll();
-            
+
             res.status(200).json(listingUser);
-        }catch(error){
+        } catch (error) {
+            res.status(500).json("An unexpected error has occurred!");
+        }
+    },
+    async searchingUser(req, res) {
+        try {
+            const { id } = req.params;
+
+            const searchingUser = await models.User.findAll({
+                where: {
+                    id
+                }
+            });
+
+            res.status(200).json(searchingUser);
+        } catch (error) {
             res.status(500).json("An unexpected error has occurred!");
         }
     }
