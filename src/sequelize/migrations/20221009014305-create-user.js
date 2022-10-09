2fda253dfc88e0aa0a0d91eeb9df5ed27ea245eb
mateusmaciel340 <mateusmaciel340@gmail.com>
2022-10-08 22:52:34 -0300

    feat: add migration user

---
+++ src/sequelize/migrations/20221009014305-create-user.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        defaultValue: Sequelize.UUUID4,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      username: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(80),
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
